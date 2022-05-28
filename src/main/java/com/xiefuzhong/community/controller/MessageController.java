041e243b3528240377d59104ca94e4108c9f86cb
xiefuzhongo <2573348471@qq.com>
2022-05-28 10:33:45 +0800

    展示私信页，显示私信信息已完成

---
+++ src/main/java/com/xiefuzhong/community/controller/MessageController.java
@@ -0,0 +1,154 @@
+package com.xiefuzhong.community.controller;
+
+
+import com.xiefuzhong.community.entity.Message;
+import com.xiefuzhong.community.entity.Page;
+import com.xiefuzhong.community.entity.User;
+import com.xiefuzhong.community.service.UserService;
+import com.xiefuzhong.community.service.impl.MessageServiceImpl;
+import com.xiefuzhong.community.util.CommunityUtil;
+import com.xiefuzhong.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
+
+@Controller
+public class MessageController {
+
+    @Autowired
+    private MessageServiceImpl messageService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
+
+    // 私信列表
+    @RequestMapping(path = "/letter/list", method = RequestMethod.GET)
+    public String getLetterList(Model model, Page page) {
+        User user = hostHolder.getUser();
+        // 分页信息
+        page.setLimit(5);
+        page.setPath("/letter/list");
+        page.setRows(messageService.findConversationCount(user.getId()));
+
+        // 会话列表
+        List<Message> conversationList = messageService.findConversations(
+                user.getId(), page.getOffset(), page.getLimit());
+        //每一个会话列表都添加数据后封装为map,再将map存到list中
+        List<Map<String, Object>> conversations = new ArrayList<>();
+        if (conversationList != null) {
+            for (Message message : conversationList) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("conversation", message);
+                map.put("letterCount", messageService.findLetterCount(message.getConversationId()));
+                map.put("unreadCount", messageService.findLetterUnreadCount(user.getId(), message.getConversationId()));
+                //我给别人发，显示接受者的头像；别人给我发，显示的是发送人的头像
+                int targetId = user.getId() == message.getFromId() ? message.getToId() : message.getFromId();
+                map.put("target", userService.findUserById(targetId));
+
+                conversations.add(map);
+            }
+        }
+        model.addAttribute("conversations", conversations);
+
+        // 查询整个用户所有的未读消息数量
+        int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
+        model.addAttribute("letterUnreadCount", letterUnreadCount);
+
+        return "/site/letter";
+    }
+
+    //私信详情
+    @RequestMapping(path = "/letter/detail/{conversationId}", method = RequestMethod.GET)
+    public String getLetterDetail(@PathVariable("conversationId") String conversationId, Page page, Model model) {
+        // 分页信息
+        page.setLimit(5);
+        page.setPath("/letter/detail/" + conversationId);
+        page.setRows(messageService.findLetterCount(conversationId));
+
+        // 私信列表
+        List<Message> letterList = messageService.findLetters(conversationId, page.getOffset(), page.getLimit());
+        //补充私信有关信息
+        List<Map<String, Object>> letters = new ArrayList<>();
+        if (letterList != null) {
+            for (Message message : letterList) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("letter", message);
+                map.put("fromUser", userService.findUserById(message.getFromId()));
+                letters.add(map);
+            }
+        }
+        model.addAttribute("letters", letters);
+
+        // 私信目标
+        model.addAttribute("target", getLetterTarget(conversationId));
+
+        // 设置已读
+        List<Integer> ids = getLetterIds(letterList);
+        if (!ids.isEmpty()) {
+            messageService.readMessage(ids);
+        }
+
+        return "/site/letter-detail";
+    }
+
+    //查询目标对象信息
+    private User getLetterTarget(String conversationId) {
+        String[] ids = conversationId.split("_");
+        int id0 = Integer.parseInt(ids[0]);
+        int id1 = Integer.parseInt(ids[1]);
+
+        //两端都是用户id,所以如果一端一样，那么就需要获取另外一端的用户数据
+        if (hostHolder.getUser().getId() == id0) {
+            return userService.findUserById(id1);
+        } else {
+            return userService.findUserById(id0);
+        }
+    }
+
+    private List<Integer> getLetterIds(List<Message> letterList) {
+        List<Integer> ids = new ArrayList<>();
+
+        if (letterList != null) {
+            for (Message message : letterList) {
+                if (hostHolder.getUser().getId() == message.getToId() && message.getStatus() == 0) {
+                    ids.add(message.getId());
+                }
+            }
+        }
+
+        return ids;
+    }
+
+//    @RequestMapping(path = "/letter/send", method = RequestMethod.POST)
+//    @ResponseBody
+//    public String sendLetter(String toName, String content) {
+//        User target = userService.findUserByName(toName);
+//        if (target == null) {
+//            return CommunityUtil.getJSONString(1, "目标用户不存在!");
+//        }
+//
+//        Message message = new Message();
+//        message.setFromId(hostHolder.getUser().getId());
+//        message.setToId(target.getId());
+//        if (message.getFromId() < message.getToId()) {
+//            message.setConversationId(message.getFromId() + "_" + message.getToId());
+//        } else {
+//            message.setConversationId(message.getToId() + "_" + message.getFromId());
+//        }
+//        message.setContent(content);
+//        message.setCreateTime(new Date());
+//        messageService.addMessage(message);
+//
+//        return CommunityUtil.getJSONString(0);
+//    }
+
+}
